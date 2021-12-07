66444418d63aac8ab3e692dae02133cbe1e23194
DmitryChuhnin <dmitrychuhnin@icloud.com>
2021-12-07 12:19:03 +0300

    Font without stupid

--- Shop/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from Shop.models import ProductColor
-
-
-class ProductColorForm(ModelForm):
-    class Meta:
-        model = ProductColor
-        fields = '__all__'
