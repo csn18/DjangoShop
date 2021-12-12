4f35c4817b302dd301139a13c8957e519963628c
forest <formcsp@gmail.com>
2021-12-12 17:20:53 +0300

    Added Swagger for get API list

--- Backend/DjangoSeller/urls.py
+++ Backend/DjangoSeller/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
 from Accounts.router import urlpatterns as url_accounts
 from Shop.router import urlpatterns as url_shop
 
+api_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('all-api/', api_view)
 ]
 
 urlpatterns += url_accounts
