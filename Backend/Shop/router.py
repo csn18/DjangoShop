575d71edcca03e9ec5b0b190ee3bca973954ffd3
forest <formcsp@gmail.com>
2021-12-12 16:45:50 +0300

    Added API for create and login user with JWT token Update .gitignore, setting.py and urls.py files

---
+++ Backend/Shop/router.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from Shop.api import *
+
+router = DefaultRouter()
+router.register(r'products', AllProductViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
